eacf024d236af377b896788b7153ec1a1d875776
Andrey Smorodov <andreysmorodov@list.ru>
2014-01-05 17:37:27 +0400

    uncommented visualizemodel line

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
 {
 	Model model("..\\..\\Model\\",13,146,710);
 	
-	// visualizemodel(model,0,13);
+	visualizemodel(model,0,13);
 	model.interval=5;
 	model.thresh=MIN(-0.65,model.thresh);
 	
